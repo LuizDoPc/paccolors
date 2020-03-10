40c054ae9d40c73c0f8f7c7c3d442c443b714ea5
luizosoares <luizotavio@ioasys.com.br>
2020-03-10 12:33:56 -0300

    frontend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-app.use('/', (req, res) => {
+app.use(cors());
+app.use(express.json());
+
+app.post('/', (req, res) => {
     const { colorA, colorB } = req.body;
     io.emit('colors', [colorA, colorB]);
-})
+    return res.status(200).send();
+});
+
 
 server.listen(3333);
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [colorA, setColorA] = useState('');
+  const [colorB, setColorB] = useState('');
+  return (
+    <div style={{display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+      <input value={colorA} onChange={(e) => setColorA(e.target.value)} style={{ fontSize: 30 }}/>
+      <input value={colorB} onChange={(e) => setColorB(e.target.value)} style={{ marginTop: '1%', fontSize: 30 }} />
+      <button style={{ marginTop: '1%', fontSize: 30 }} onClick={() => {
+        axios.post('http://localhost:3333', {
+          colorA,
+          colorB
+        })        
+      }} >GO</button>
+    </div>
+  );
+}
+
+export default App;
