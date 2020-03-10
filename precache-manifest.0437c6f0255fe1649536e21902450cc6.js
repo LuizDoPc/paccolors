4373519200c8d6e684e586213b255a598b08ebc8
luizosoares <luizotavio@ioasys.com.br>
2020-03-10 13:28:50 -0300

    Updates

---
+++ precache-manifest.0437c6f0255fe1649536e21902450cc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8219254fcfbf628d93119a2ecd81014",
+    "url": "/paccolors/index.html"
+  },
+  {
+    "revision": "2ed21b4e796a81acbd8e",
+    "url": "/paccolors/static/js/2.50d53d6a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/paccolors/static/js/2.50d53d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa990f5f5ce3cacfc182",
+    "url": "/paccolors/static/js/main.3a5748be.chunk.js"
+  },
+  {
+    "revision": "068a2bb56cb0025aea7f",
+    "url": "/paccolors/static/js/runtime-main.8e918767.js"
+  }
+]);
